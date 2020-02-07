facebed6be9ca859f4f730b9dbbcdc185e170bef
Thatoneguyouknow <benjistaman.12@gmail.com>
2020-02-07 16:48:57 -0700

    Add files via upload

--- GradientDescent.py
+++ GradientDescent.py
@@ -1,11 +1,47 @@
 import numpy as np
+import csv
+
+
+def main():
+    filename = input("Input filename here: ")
+    readInputs(filename)
+    
+    pass
+
+# iterates through .data files and splits input and output data into x and y respectively
+def readInputs(dataFile):
+    dataF = open(dataFile, 'r')
+    csv_reader = csv.reader(dataF, delimiter=' ')
+    x = []
+    y = []
+    endCol = 0
+    for row in csv_reader:
+        endCol = len(row) - 1
+        print(endCol)
+        counter = 0
+        tempArr = []
+        while(counter < len(row)):
+            if(counter == endCol):
+                y.append(row[counter))
+                counter += 1
+            else:
+                tempArr.append(row[counter])
+                counter += 1
+        x.append(tempArr)
+    GradientDescent(x, y, 0.1)
+    pass
 
 def GradientDescent(x,y, stepSize):
     currentM = 0
 	currentB = 0
     maxIterations = 500 #dummy value
     n = len(x)
-    stepSize = 0.01 #dummy value
+
+    #initialize weightVector to all 0s for each feature
+    weightVector = []
+    for feature in x[0]:
+        weightVector.append(0)
+    weightMatrix = []
 
     for num in range(maxIterations):
         predY = currentM * x + currentB
@@ -16,7 +52,6 @@ def GradientDescent(x,y, stepSize):
         currentB = currentB - stepSize * bDer
         print ("m {}, b {}, cost {} iteration {}".format(currentM,currentB,cost, i))
 
-x = np.array([]) #fill with data set numbers
-y = np.array([]) #I don't know how to do this
+    return weightMatrix
 
-gradient_descent(x,y)
+main()
