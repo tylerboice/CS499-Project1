4ea80e9353d2587b9152b6bee3443d4ba632f702
Tyler Boice <tylerboice@users.noreply.github.com>
2020-02-07 21:57:17 -0700

    Plotting added

--- GradientDescent.py
+++ GradientDescent.py
@@ -42,6 +42,8 @@ def gradient_descent(x,y, stepSize, maxIterations):
 
 # Opens a csv file and places values into x and y
 def open_csv_file(filename):
+    x = [[]]
+    y = []
     # open and read a csv file
     with open(filename, 'rt') as file:
         freader = csv.reader(file)
@@ -53,12 +55,12 @@ def open_csv_file(filename):
         for row in freader:
             csvrows.append(row)
 
-        x = [[] for _ in range(len(csvrows))]
+        x = [[] for _ in range(0, len(csvrows))]
 
     # Separate data into x and y matricies
     itercount = 0
     for line in csvrows:
-        for varcount in range(0, len(line)):
+        for varcount in range(len(line)):
             if varcount is not len(line) - 1:
                 x[itercount].append(line[varcount])
             else:
@@ -90,6 +92,15 @@ def data_splitter(x):
 # Scales data from csv file
 
 if __name__ == '__main__':
+    # Get data from csv file
     x, y = open_csv_file("SAheart.data.csv")
+
+    #run gradient desecent on data set
+    # gradient_descent(x, y, 0.1, 500)
+
+    #split data set in to 3 sections
     train, validate, test = data_splitter(x)
-    #gradient_descent(x, y, 0.1, 500)
+
+    # Plot data points
+    plt.plot(x, y)
+    plt.show()
